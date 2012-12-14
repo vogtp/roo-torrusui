ad3f3e0c34ab21afa6ae5c4590c7eea7576f5de9
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-12-14 21:40:44 +0100

    finde example

--- src/main/java/ch/unibas/roo/torrus/server/domain/Device.java
+++ src/main/java/ch/unibas/roo/torrus/server/domain/Device.java
@@ -8,10 +8,36 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
+// @RooEntity(finders = { "findDevicesByName" })
 public class Device {
 
 	@NotBlank
 	private String name;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Device other = (Device) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 	// public static List Topping.findAllToppings() {
 	// return entityManager().createQuery("SELECT o FROM Topping o",
